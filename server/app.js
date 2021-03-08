7a5e50ec1eda9f1ea0d0276e368314d76e226769
Rajput-Suraj <surajarajput@gmail.com>
2021-03-08 18:58:56 +0530

    Added CORS to allow cross-origin-access to application

--- server/app.js
+++ server/app.js
@@ -1,19 +1,27 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 
 const app = express();
 
+//ALLOW CROSS-ORIGIN REQUESTS
+app.use(cors());
+
+//CONNECT MONGODB
 mongoose.connect(process.env.MONGODB_CONNECT, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
+//CHECK WHETHER DB CONNECTION IS ESTABLISHED OR NOT
 mongoose.connection.once("open", () => {
     console.log("Connected to DB");
 });
 
+//API ROUTE/ENDPOINT
 app.use(
     "/graphql",
     graphqlHTTP({
