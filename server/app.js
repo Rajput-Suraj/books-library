b15467475b660f40301c3df831c33f1e2ab004e1
Rajput-Suraj <surajarajput@gmail.com>
2021-03-08 15:04:27 +0530

    Connection is established between application and mongodb

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+    console.log("Connected to DB");
+});
 
 app.use(
     "/graphql",
